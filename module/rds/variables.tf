20fe3e0717531484238fdece907411941b60211a
Narendra T <narendratarra935@gmail.com>
2024-01-30 14:03:55 +0530

    TF Code

---
+++ module/rds/variables.tf
@@ -0,0 +1,8 @@
+variable "allocated_storage" {}
+variable "db_name" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "instance_class" {}
+variable "username" {}
+variable "password" {}
+variable "family" {}
