ac9615a3c5efb243d9fec845666152dd27d654f7
Narendra T <narendratarra935@gmail.com>
2024-01-30 18:43:06 +0530

    TF Code

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -3,6 +3,7 @@ project_name = "expense"
 kms_key_id   = "arn:aws:kms:us-east-1:495277360928:key/5226d7c2-e3f9-4f4d-af88-d9226f28105e"
 
 
+/*
 vpc = {
   main={
     vpc_cidr             ="10.10.0.0/21"
@@ -26,3 +27,19 @@ rds ={
 
   }
 }
+*/
+vpc_cidr             ="10.10.0.0/21"
+public_subnets_cidr  = ["10.10.0.0/25","10.10.0.128/25"]
+web_subnets_cidr     = ["10.10.1.0/25","10.10.1.128/25"]
+app_subnets_cidr     = ["10.10.2.0/25","10.10.2.128/25"]
+db_subnets_cidr      = ["10.10.3.0/25","10.10.3.128/25"]
+az                   = ["us-east-1a","us-east-1b"]
+
+
+rds_allocated_storage = 10
+rds_db_name              = "expense"
+rds_engine               = "mysql"
+rds_engine_version       = "5.7"
+rds_instance_class       = "db.t3.micro"
+rds_family               = "mysql5.7"
+
--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 module "vpc" {
   source              = "./module/vpc"
   for_each            = var.vpc
@@ -33,4 +34,40 @@ module "rds" {
   subnet_ids  = lookup(lookup(module.vpc,"main",null ), "db_subnet_ids",null)
   vpc_id      = lookup(lookup(module.vpc,"main",null ), "vpc_id",null)
   sg_cidr_blocks = lookup(lookup(var.vpc,"main",null ), "app_subnets_cidr",null)
+}*/
+
+module "vpc" {
+  source              = "./module/vpc"
+  vpc_cidr            = var.vpc_cidr
+  public_subnets_cidr = var.public_subnets_cidr
+  web_subnets_cidr    = var.web_subnets_cidr
+  app_subnets_cidr    = var.app_subnets_cidr
+  db_subnets_cidr     = var.db_subnets_cidr
+  az                  = var. az
+
+  env                 = var.env
+  project_name        = var.project_name
+}
+
+module "rds" {
+  source = "./module/rds"
+
+
+  ## varible of rds
+  allocated_storage    = var.rds_allocated_storage
+  db_name              = var.rds_db_name
+  engine               = var.rds_engine
+  engine_version       =var.rds_engine_version
+  instance_class       = var.rds-instance_class
+  family               = var.rds_family
+
+  ##common varibles
+  env                 = var.env
+  project_name        = var.project_name
+  kms_key_id          = var.kms_key_id
+
+  ##other varibles like outputs
+  subnet_ids  = module.vpc.app_subnets_ids
+  vpc_id      = module.vpc.vpc_id
+  sg_cidr_blocks = var.app_subnets_cidr
 }
