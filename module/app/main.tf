a4f677ecf138cf7bd13c137b37a99b41192f30d2
Narendra T <narendratarra935@gmail.com>
2024-01-31 13:09:35 +0530

    TF Code

--- module/app/main.tf
+++ module/app/main.tf
@@ -1,3 +1,4 @@
+
 resource "aws_security_group" "main" {
   name        = "${local.name}-sg"
   description = "${local.name}-sg"
@@ -9,29 +10,34 @@ resource "aws_security_group" "main" {
     protocol         = "tcp"
     cidr_blocks      = var.bastion_cidrs
     description      = "SSH"
+
   }
 
   ingress {
-    from_port   = var.app_port
-    to_port     = var.app_port
-    protocol    = "tcp"
-    cidr_blocks = var.sg_cidr_blocks
-    description = "APPPORT"
+    from_port        = var.app_port
+    to_port          = var.app_port
+    protocol         = "tcp"
+    cidr_blocks      = var.sg_cidr_blocks
+    description      = "SSH"
+
   }
 
-    egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
 
-
-    tags = {
-      Name = "${local.name}-sg"
-    }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
+
+
+  tags = {
+    Name =  "${local.name}}-sg"
+  }
+}
+
+
 resource "aws_launch_template" "main" {
   name_prefix   = "${local.name}-lt"
   image_id      = data.aws_ami.centos8.image_id
@@ -40,15 +46,16 @@ resource "aws_launch_template" "main" {
 }
 
 resource "aws_autoscaling_group" "main" {
-  name                  = "${local.name}-asg"
-  desired_capacity      = var.instance_capacity
-  max_size              = var.instance_capacity
-  min_size              = var.instance_capacity
-  vpc_zone_identifier   = var.vpc_zone_identifier
+  name               = "${local.name}-asg"
+  desired_capacity   = var.instance_capacity
+  max_size           = var.instance_capacity #TBD this we will fine tune after autoscaling
+  min_size           = var.instance_capacity
+  vpc_zone_identifier = var.vpc_zone_identifier
 
   launch_template {
     id      = aws_launch_template.main.id
-    version ="$latest"
+    version = "$Latest"
+
   }
 
   tag {
@@ -57,4 +64,6 @@ resource "aws_autoscaling_group" "main" {
     propagate_at_launch = true
   }
 }
+
+
 
